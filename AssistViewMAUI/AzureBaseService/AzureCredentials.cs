f3ccb9aa10478c9e87e8711fb254d2192af0dc5d
Saravanan Madheswaran <saravanan.madheswaran@syncfusion.com>
2025-04-15 21:38:25 +0530

    committed.

---
+++ AssistViewMAUI/AzureBaseService/AzureCredentials.cs
@@ -0,0 +1,9 @@
+﻿namespace AssistViewMAUI
+{
+    internal static class AzureCredentials
+    {
+        internal const string endpoint = "https://mobilemaui.openai.azure.com/";
+        internal const string deploymentName = "gpt-4o";
+        internal const string key = "6673b6975f334c79bd0db8a1cd70aa49";
+    }
+}
